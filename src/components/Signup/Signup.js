7b5f9d60caca282e30d81a9e50362d791c9368cd
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-15 21:24:20 +0600

    signup form added

--- src/components/Signup/Signup.js
+++ src/components/Signup/Signup.js
@@ -3,26 +3,52 @@ import { Button, Form } from "react-bootstrap";
 
 const Signup = () => {
   return (
-    <Form className="text-start m-5">
-      <Form.Group className="mb-3" controlId="formBasicEmail">
-        <Form.Label>Email address</Form.Label>
-        <Form.Control type="email" placeholder="Enter email" />
-        <Form.Text className="text-muted">
-          We'll never share your email with anyone else.
-        </Form.Text>
-      </Form.Group>
+    <div className="d-flex justify-content-center  mt-5">
+      <Form className="text-start m-5 w-50">
+        <Form.Group className="mb-3" controlId="formBasicName">
+          <Form.Control
+            type="text"
+            placeholder="Enter Name"
+            className="p-3"
+            style={{ background: "lightGrey" }}
+          />
+        </Form.Group>
+        <Form.Group className="mb-3" controlId="formBasicEmail">
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            className="p-3"
+            style={{ background: "lightGrey" }}
+          />
+        </Form.Group>
 
-      <Form.Group className="mb-3" controlId="formBasicPassword">
-        <Form.Label>Password</Form.Label>
-        <Form.Control type="password" placeholder="Password" />
-      </Form.Group>
-      <Form.Group className="mb-3" controlId="formBasicCheckbox">
-        <Form.Check type="checkbox" label="Check me out" />
-      </Form.Group>
-      <Button variant="primary" type="submit">
-        Submit
-      </Button>
-    </Form>
+        <Form.Group className="mb-3" controlId="formBasicPassword">
+          <Form.Control
+            type="password"
+            placeholder="Password"
+            className="p-3"
+            style={{ background: "lightGrey" }}
+          />
+        </Form.Group>
+
+        <Form.Group className="mb-3" controlId="formBasicConfirmPassword">
+          <Form.Control
+            type="password"
+            placeholder="Confirm Password"
+            className="p-3"
+            style={{ background: "lightGrey" }}
+          />
+        </Form.Group>
+
+        <Button
+          variant="primary"
+          type="submit"
+          className="w-100 p-3 bg-danger text-light border-0"
+        >
+          Signup
+        </Button>
+      </Form>
+    </div>
   );
 };
 
