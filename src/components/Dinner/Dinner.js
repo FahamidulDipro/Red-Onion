10f913a553e57d44d72e529add57d5b0a73be977
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-15 17:18:00 +0600

    all json data image url fixed

--- src/components/Dinner/Dinner.js
+++ src/components/Dinner/Dinner.js
@@ -2,13 +2,18 @@ import React from "react";
 import { Button, Card, Col } from "react-bootstrap";
 
 const Dinner = ({ dinner }) => {
+  const { name, picture, price } = dinner;
   return (
     <Col lg={4} className="mt-5 ">
       <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src={picture} />
         <Card.Body>
           <Card.Title>{dinner.name}</Card.Title>
           <Card.Text></Card.Text>
+          <p>
+            <b>Price: </b>
+            {price}
+          </p>
           <Button variant="primary">Go somewhere</Button>
         </Card.Body>
       </Card>
--- src/components/Lunch/Lunch.js
+++ src/components/Lunch/Lunch.js
@@ -2,17 +2,21 @@ import React, { useContext } from "react";
 import { Button, Card, Col } from "react-bootstrap";
 import { FoodLoad } from "../../App";
 
-const Lunch = (props) => {
-  const { lunch } = props;
+const Lunch = ({ lunch }) => {
+  const { name, picture, price } = lunch;
 
   return (
     <Col lg={4} className="mt-5">
       <Card style={{ width: "18rem" }}>
-        <Card.Img variant="top" src="holder.js/100px180" />
+        <Card.Img variant="top" src={picture} />
         <Card.Body>
           <Card.Title>{lunch.name}</Card.Title>
           <Card.Text></Card.Text>
           <Button variant="primary">Go somewhere</Button>
+          <p>
+            <b>Price: </b>
+            {price}
+          </p>
         </Card.Body>
       </Card>
     </Col>
