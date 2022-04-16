b8e315f37de0f1cbdd920267fe3bc6ff6a0866c1
FahamidulDipro <mdfahamidulislam047@gmail.com>
2022-04-16 21:09:42 +0600

    food details are shown with picture in details page

--- src/components/Detail/Detail.js
+++ src/components/Detail/Detail.js
@@ -1,4 +1,5 @@
 import React, { useContext } from 'react';
+import { Button } from 'react-bootstrap';
 import { useParams } from 'react-router-dom';
 import { FoodLoad } from '../../App';
 
@@ -13,11 +14,17 @@ const Detail = () => {
   
 
     return (
-        <div>
-            <h1 className='mt-5'>This is detail</h1>
-            {
-                console.log(foodDetail)
-            }
+        <div className='container d-flex justify-content-between ' style={{marginTop:'200px'}}>
+          <div className='w-50 mt-5 text-start'>
+              <h1>{name}</h1>
+              <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Numquam, expedita. Veniam quibusdam officiis labore aperiam, est, aliquid doloremque tenetur architecto beatae at iste nisi repellendus ut in sapiente voluptas facilis?</p>
+              <p style={{fontSize:'35px'}}><b> ${price}</b><input type="text" name="" id="" className='mx-3 rounded-pill p-2 w-25'/></p>
+              <Button variant='danger' className='px-5 rounded-pill'style={{fontSize:'25px'}}>Add</Button>
+          </div>
+            <div className='w-50'>
+             <img src={picture} alt="foodPic" className='img-fluid'/>
+           
+            </div>
         </div>
     );
 };
